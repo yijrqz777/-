fdcf99779e2a5bb9d1d4b7d046bb1131cd05683a
yjrqz777 <3210551161@qq.com>
2022-01-09 18:13:00 +0800

    第四次测试

--- upload.bat
+++ upload.bat
@@ -1,17 +1,17 @@
 start git add .
 
-choice /t 1 /d y /n >nul
+choice /t 2 /d y /n >nul
 
-start git commit -m "ç¬¬ä¸‰æ¬¡æµ‹è¯•"
+start git commit -m "µÚËÄ´Î²âÊÔ"
 
-choice /t 1 /d y /n >nul
+choice /t 2 /d y /n >nul
 
 start git branch -M main
 
-choice /t 1 /d y /n >nul
+choice /t 2 /d y /n >nul
 
 start git remote add origin https://github.com/yijrqz777/Assembly-coding.git
 
-choice /t 1 /d y /n >nul
+choice /t 10 /d y /n >nul
 
 start git push -u origin main
