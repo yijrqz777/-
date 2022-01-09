09d42c5fbb6e3d4bcbbed6c7ff620d837b5b3ee0
yjrqz777 <3210551161@qq.com>
2022-01-09 18:06:00 +0800

    绗笁娆℃祴璇?

--- upload.bat
+++ upload.bat
@@ -1,1 +1,17 @@
-start git commit -m "2022-1-9-首次提交"
+start git add .
+
+choice /t 1 /d y /n >nul
+
+start git commit -m "第三次测试"
+
+choice /t 1 /d y /n >nul
+
+start git branch -M main
+
+choice /t 1 /d y /n >nul
+
+start git remote add origin https://github.com/yijrqz777/Assembly-coding.git
+
+choice /t 1 /d y /n >nul
+
+start git push -u origin main
